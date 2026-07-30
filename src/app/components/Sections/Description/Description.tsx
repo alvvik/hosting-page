e73e544b06cde73be85a5ed28c942a2d45fc2bb0
alvv <alvv2115@gmail.com>
2026-07-30 16:38:37 +0200

    feat: added description

--- src/app/components/Sections/Description/Description.tsx
+++ src/app/components/Sections/Description/Description.tsx
@@ -2,7 +2,7 @@ export default function Description() {
   return (
     <section className="flex justify-center items-center flex-col py-8">
       <h2 className="font-semibold text-fluid-h2 text-center">
-        Polski hostring serwerow minecraft
+        Polski hosting serwerów minecraft
       </h2>
       <p className="text-fluid-lead text-center max-w-xl">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel doloribus
