be0246b2277d9e15aca627f46320e866dbfbe8f8
alvv <alvv2115@gmail.com>
2026-08-02 17:41:50 +0200

    feat: added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,8 @@ export default function RootLayout({
           <Header />
         </div>
         {children}
+
+        <Footer />
       </body>
     </html>
   );
